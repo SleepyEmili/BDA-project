9ffc94a3c28e3a78d1f64b417e8200bacab2d91f
gzlzgzl <3447976754@qq.com>
2023-04-12 01:02:01 +0800

    logistic regression classifier

--- logistic regression.py
+++ logistic regression.py
@@ -12,17 +12,24 @@ Y = data[:,0]
 
 kf = KFold(n_splits=5)
 model = LogisticRegression(solver = "liblinear")
+acc_scores = []
+f1_scores = []
 
-acc_score = []
 for train_index, test_index in kf.split(X):
     train_X, test_X = X[train_index], X[test_index]
     train_Y, test_Y = Y[train_index], Y[test_index]
     model.fit(train_X,train_Y)
     pred_values = model.predict(test_X)
     acc = accuracy_score(pred_values , test_Y)
-    acc_score.append(acc)
+    f1 = f1_score(pred_values , test_Y)
+    acc_scores.append(acc)
+    f1_scores.append(f1)
 
-avg_acc_score = sum(acc_score)/5
+avg_acc_score = sum(acc_scores)/5
+avg_f1_score = sum(f1_scores)/5
 
-print('accuracy of each fold - {}'.format(acc_score))
+print('Method: logistic regression')
+print('accuracy of each fold - {}'.format(acc_scores))
 print('Avg accuracy : {}'.format(avg_acc_score))
+print('f1 of each fold - {}'.format(f1_scores))
+print('Avg f1 : {}'.format(avg_f1_score))
