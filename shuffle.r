36ba330f2958c5386c756167795627d32e879aa0
gzlzgzl <3447976754@qq.com>
2023-04-12 23:50:40 +0800

    Merge files to Jupyter Notebook

--- shuffle.r
+++
@@ -1,4 +0,0 @@
-setwd("C:\\Users\\User\\Desktop\\Classwork\\COMP4434\\BDA-project")
-data = read.csv("virusshare.csv",header = T)
-shuffled_data = data[sample(1:nrow(data)), ]
-write.csv(shuffled_data,"virusshare2.csv", row.names = F)
